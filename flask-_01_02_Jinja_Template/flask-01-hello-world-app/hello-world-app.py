9253eddcb6fc1e49ce5488c5cf3f7c6684c6cb5c
jacob <sienholdings@gmail.com>
2025-06-27 11:42:05 +1000

    update jija files

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,17 @@
 from flask import Flask 
+
 app = Flask(__name__)
 
+@app.route("/")
+def home(): 
+  return "Hello World!"
 
 
+@app.route("/bye")
+def thend(): 
+  return "<h1> Good Bye. Come Again</h1>"
 
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+  app.run(port=8080)
--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,13 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Welcome"
 
 
+@app.route("/head")
+def head():
+    render_template("index.html", Num1 = 100, Num2 = 200)
 
+@app.route("/sum")
+def sum():
 
-
+    val1 = 24
+    val2 = 33
+    mysum = val1 + val2
+    return render_template("body.html", value1 = val1, value2 = val2, sum = mysum)
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    app.run(host="0.0.0.0")
